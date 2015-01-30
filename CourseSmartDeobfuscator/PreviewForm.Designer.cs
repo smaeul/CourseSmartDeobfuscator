49e12d87eaedb080401154fad2caf81fd2bfb0bd
Samuel Holland <samuel@sholland.net>
2015-01-29 21:52:29 -0600

    Initial commit; currently working on pieces one at a time
    The code works so far, but is a bit messy.
    Original on the left, rebuilt on the right.

---
+++ CourseSmartDeobfuscator/PreviewForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CourseSmartDeobfuscator
+{
+    partial class PreviewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PreviewForm));
+            this.previewBox = new System.Windows.Forms.PictureBox();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonSave = new System.Windows.Forms.ToolStripButton();
+            this.fixedPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).BeginInit();
+            this.toolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fixedPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // previewBox
+            // 
+            this.previewBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.previewBox.Location = new System.Drawing.Point(0, 25);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(738, 972);
+            this.previewBox.TabIndex = 0;
+            this.previewBox.TabStop = false;
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButtonSave});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(1476, 25);
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Open Image";
+            this.toolStripButton1.ToolTipText = "Open Image";
+            this.toolStripButton1.Click += new System.EventHandler(this.openImage);
+            // 
+            // toolStripButtonSave
+            // 
+            this.toolStripButtonSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonSave.Name = "toolStripButtonSave";
+            this.toolStripButtonSave.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonSave.Text = "Save Map";
+            this.toolStripButtonSave.Click += new System.EventHandler(this.saveMap);
+            // 
+            // fixedPictureBox
+            // 
+            this.fixedPictureBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.fixedPictureBox.Location = new System.Drawing.Point(738, 25);
+            this.fixedPictureBox.Name = "fixedPictureBox";
+            this.fixedPictureBox.Size = new System.Drawing.Size(738, 972);
+            this.fixedPictureBox.TabIndex = 2;
+            this.fixedPictureBox.TabStop = false;
+            // 
+            // PreviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 997);
+            this.Controls.Add(this.fixedPictureBox);
+            this.Controls.Add(this.previewBox);
+            this.Controls.Add(this.toolStrip);
+            this.Name = "PreviewForm";
+            this.Text = "Preview";
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).EndInit();
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fixedPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox previewBox;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonSave;
+        private System.Windows.Forms.PictureBox fixedPictureBox;
+    }
+}
+
